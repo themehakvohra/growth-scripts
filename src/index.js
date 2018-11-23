2b14a33436404e6e05ac6fd3c040fc475ed6cc6a
Mehak Vohra <watthemehak@gmail.com>
2018-11-22 22:13:55 -0500

    authentication added

--- src/index.js
+++ src/index.js
@@ -1,32 +1,28 @@
 const { GraphQLServer } = require('graphql-yoga')
+const { Prisma } = require('prisma-binding')
 
-let links = [{
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL'
-}]
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const AuthPayload = require('./resolvers/AuthPayload')
 
-let idCount = links.length
 const resolvers = {
-    Query: {
-        info: () => `This is the API of a Hackernews Clone`,
-        feed: () => links,
-    },
-    Mutation: {
-        post: (root, args) => {
-            const link = {
-                id: `link-${idCount++}`,
-                description: args.description,
-                url: args.url,
-            }
-            links.push(link)
-            return link
-        }
-    },
+    Query,
+    Mutation,
+    AuthPayload
 }
 
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
     resolvers,
+    context: req => ({
+        ...req,
+        db: new Prisma({
+            typeDefs: 'src/generated/prisma.graphql',
+            endpoint: 'https://us1.prisma.sh/mehak-vohra-204100/database/dev',
+            secret: 'mysecret123',
+            debug: true,
+        }),
+    }),
 })
+
 server.start(() => console.log(`Server is running on http://localhost:4000`))
