068fb1593c9a63fa0a0380260133e8d70c6d3797
Mehak Vohra <watthemehak@gmail.com>
2018-11-23 04:19:16 -0500

    finished backend

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ const { Prisma } = require('prisma-binding')
 const Query = require('./resolvers/Query')
 const Mutation = require('./resolvers/Mutation')
 const AuthPayload = require('./resolvers/AuthPayload')
+const Subscription = require('./resolvers/Subscription')
+const Feed = require('./resolvers/Feed')
 
 const resolvers = {
     Query,
     Mutation,
-    AuthPayload
+    AuthPayload,
+    Subscription,
+    Feed
 }
 
 const server = new GraphQLServer({
