2d8e1be63a4291299561f3d4b747a544468020f6
Mehak Vohra <watthemehak@gmail.com>
2018-11-23 20:00:06 -0500

    require authentication on the server side

--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -34,15 +34,15 @@ async function login(parent, args, context, info) {
     }
 }
 
-function post(parent, args, context, info) {
+function post(parent, { url, description }, context, info) {
     const userId = getUserId(context)
     return context.db.mutation.createLink(
         {
             data: {
-                url: args.url,
-                description: args.description,
-                postedBy: { connect: { id: userId } },
-            },
+                url,
+                description,
+                postedBy: { connect: { id: userId } }
+            }
         },
         info,
     )
